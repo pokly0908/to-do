273f8c958d8961355c7da4c6f56a8ce80b975ffe
pokly0908 <pokly0908@naver.com>
2024-02-05 02:19:01 +0900

    :recycle: good

--- src/main/java/com/sparta/todo/dto/UserRequestDto.java
+++ src/main/java/com/sparta/todo/dto/UserRequestDto.java
@@ -10,11 +10,11 @@ public class UserRequestDto {
 
     @NotBlank
     @Size(min = 4, max = 10, message = "4자 이상 10자 이하로 입력하세요.")
-    @Pattern(regexp = "^[a-z0-9]+$", message = "알파벳 소문자, 숫자로만 구성되어야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "알파벳 소문자와 숫자만 입력 가능합니다")
     private String username;
 
     @Size(min = 8, max = 15, message = "8자 이상 15자 이하로 입력하세요.")
-    @Pattern(regexp = "^[a-z0-9]+$", message = "알파벳 소문자, 숫자로만 구성되어야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "알파벳 소문자와 숫자만 입력 가능합니다")
     @NotBlank
     private String password;
 
