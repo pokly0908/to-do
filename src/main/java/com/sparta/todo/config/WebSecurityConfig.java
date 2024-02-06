46daa4729a38478d7d4e6563102df5cf68ec2c32
pokly0908 <pokly0908@naver.com>
2024-02-06 13:31:38 +0900

    :recycle: config 수정

--- src/main/java/com/sparta/todo/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/todo/config/WebSecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -69,5 +71,10 @@ public class WebSecurityConfig {
         http.addFilterBefore(new LoggingFilter(), JwtAuthorizationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
